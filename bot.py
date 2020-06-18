6ea93b902029cc47c277b3443b36dbd882fc73d1
FedorovVladimir <vladimir.fodorow@gmail.com>
2020-06-19 00:57:00 +0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,52 @@
+import json
+import telebot
+
+from site_parser import parse
+
+config_file_name = 'telegram_token.json'
+try:
+    with open(config_file_name, 'r') as file:
+        data = file.read()
+except FileNotFoundError as e:
+    print(f"File {config_file_name} not found.")
+    exit()
+config = json.loads(data)
+
+bot = telebot.TeleBot(config["token"])
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Привет я bot для статистики конкурса "Будущее России"" от 3CRABS soft!\n'
+                                      'Чтобы узнать подробности введи /help')
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.chat.id, "Команды:\n"
+                                      "/start - привет бот\n"
+                                      "/help - помощь\n"
+                                      "Бот - показ всего списка\n"
+                                      "Бот {количество записей} - показ части списка\n")
+
+
+def create_answer(text: str, chat_id):
+    if 'бот' == text:
+        bot.send_message(chat_id, 'Ищу всех участников')
+        return parse()
+    elif text.startswith('бот'):
+        count = int(text.replace('бот', '').strip())
+        bot.send_message(chat_id, f'Ищу {count} участников')
+        return parse(count)
+
+
+@bot.message_handler(content_types=["text"])
+def content_text(message):
+    chat_id = message.chat.id
+    answer = create_answer(message.text.lower(), chat_id)
+    if answer is not None:
+        bot.send_message(chat_id, answer)
+
+
+if __name__ == '__main__':
+    bot.polling()
---
+++ site_parser.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class User:
+
+    def __init__(self, name: str, count: int):
+        self.name = name
+        self.count = count
+
+    def __str__(self):
+        return f'{self.count} {self.name}'
+
+    def __gt__(self, other):
+        return self.count > other.count
+
+
+def parse(max_count=None):
+    url = 'https://доблестьалтая.рф/voiting'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    # получаем количество страниц
+    count_pages = int(soup.find('div', class_='pagination').find_all('a')[-1].text)
+
+    # обработка по одной странице
+    users = []
+    for i in range(count_pages):
+        url = f'https://доблестьалтая.рф/voiting/?PAGEN_1={i + 1}'
+        try:
+            response = requests.get(url)
+        except ConnectionError:
+            return 'Не удалось получить доступ к сайту, попробуйте позже'
+        soup = BeautifulSoup(response.text, 'lxml')
+        cards = soup.find_all('div', class_='video_name')
+        for card in cards:
+            name = card.find_all('strong')[0].text
+            count = int(card.find_all('span')[1].text)
+            users.append(User(name, count))
+
+    users.sort(reverse=True)
+    text = ''
+    for i in range(len(users)):
+        if i == max_count:
+            break
+        text += f'{str(users[i])}\n'
+
+    return text
+
+
+if __name__ == '__main__':
+    print(parse())
